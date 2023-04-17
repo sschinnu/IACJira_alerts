e7d9af1d0a309c5ce6fe2b6871dd3dbc83fa1618
StephyRG <107910964+StephyRG@users.noreply.github.com>
2023-04-17 12:29:11 +0530

    Update s3_mfadelete.tf

--- s3_mfadelete.tf
+++ s3_mfadelete.tf
@@ -16,3 +16,14 @@ resource "aws_s3_bucket" "mfadelete8" {
   }
 }
 
+resource "aws_s3_bucket" "mfadelete9" {
+  bucket = "examplebuckettftest9"
+  acl    = "private"
+  versioning {
+    enabled = true
+    mfa_delete = true
+  }
+}
+
+
+
